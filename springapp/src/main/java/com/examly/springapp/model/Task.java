20fe24464e75694d23d57614b3160de13d81a1bc
Madhuchandhana19 <madhuchandhana19@gmail.com>
2025-12-15 10:49:09 +0000

    day 4 on progress

--- springapp/src/main/java/com/examly/springapp/model/Task.java
+++ springapp/src/main/java/com/examly/springapp/model/Task.java
@@ -1,5 +1,16 @@
 package com.examly.springapp.model;
+import java.util.*;
+import jakarta.persistence.*;
 
+@Entity
 public class Task {
+    @Id
+    private long taskId;
+    private String title;
+    private String description;
+    private String status;
+    private String priority;
+    private date dueDate;
     
+
 }
